732c34350706d9b443aef63f0ff9387e5498dc6f
toshiakisuzuki <flaflabell@gmail.com>
2020-10-26 20:07:19 +0900

    meka dynamic ejs

--- chapter2/app_ejs.js
+++ chapter2/app_ejs.js
@@ -10,7 +10,10 @@ server.listen('3000');
 console.log('Server start!');
 
 function getFromClient(request, response) {
-  var content = ejs.render(index_page);
+  var content = ejs.render(index_page, {
+    title: "Index",
+    content: "これはテンプレートを使ったサンプルページです"
+  });
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write(content);
   response.end()
