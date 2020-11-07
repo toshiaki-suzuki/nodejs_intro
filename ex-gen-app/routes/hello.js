3a22a2f0488a1d7ad202d7b0b4e16b5df0986939
Toshiaki Suzuki <tsu@marx.local>
2020-11-07 10:10:06 +0900

    use session in ex-gen-app

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -3,21 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var name = req.query.name;
-  var mail = req.query.mail;
-  var data = { 
+  var msg = '※何か書いて送信してください';
+  if (req.session.message != undefined) {
+    msg = "Last Message: " + req.session.message;
+  }
+  var data = {
     title: 'Hello!',
-    content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは' + mail + 'です。'
+    content: msg
   };
   res.render('hello', data);
 });
 
 router.post('/post', function(req, res, next) {
-  // リクエストパラメータの取り出し方
   var msg = req.body['message'];
+  req.session.message = msg;
   var data = {
     title: 'Hello!',
-    content: 'あなたは、「' + msg + '」と送信しました。'
+    content: 'Last Message: ' + req.session.message
   };
   res.render('hello', data);
 });
