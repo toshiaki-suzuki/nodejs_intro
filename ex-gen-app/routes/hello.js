6d910b2b17eff9b1742d1a6ba12f497d4e39ec99
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-10 19:31:27 +0900

    use mysql in ex-gen-app

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,30 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-var http = require('https');
-var parseString = require('xml2js').parseString;
+var mysql_setting = {
+  host: '192.168.64.2',
+  port: '3306',
+  user: 'admin',
+  password: 'password',
+  database: 'my-nodeapp-db',
+};
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  var opt = {
-    host: 'news.google.com',
-    port: 443,
-    path: '/rss?hl=ja&gl=JP&ceid=JP:ja'
-  };
-  http.get(opt, (res2)=> {
-    var body = '';
-    res2.on('data', (data)=> {
-      body += data;
-    });
-    res2.on('end', ()=> {
-      parseString(body.trim(), (err, result)=> {
-        var data = {
-          title: 'Hello!',
-          content: result.rss.channel[0].item
-        };
-        res.render('hello', data);
-      });
-    });
+  var connection = mysql.createConnection(mysql_setting);
+  // dbに接続
+  connection.connect();
+
+  // データ取得→取得後の処理
+  connection.query('SELECT * from mydata', function(error, results, fields) {
+    if (error == null) {
+      var data = {title: 'mysql', content: results};
+      res.render('hello', data);
+    }
+    console.log(error);
+    connection.end();
   });
 });
 
