34bf212faf27d67ac6438af11ee10a76aaa9eacb
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-12 20:36:11 +0900

    make edit functionaligy in hello

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -22,7 +22,7 @@ router.get('/', (req, res, next) => {
       var data = {title: 'mysql', content: results};
       res.render('hello/index', data);
     }
-    console.log(error);
+    
     connection.end();
   });
 });
@@ -64,5 +64,38 @@ router.get('/show', (req, res, next) => {
       res.render('hello/show', data);
     }
   });
+});
+
+router.get('/edit', (req, res, next)=> {
+  var id = req.query.id;
+
+  var connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query('SELECT * from mydata where id=?', id, function(error, results, fields) {
+    if (error == null) {
+      var data = {
+        title: 'Hello/edit',
+        content: 'id = ' + id + '　のレコード：',
+        mydata: results[0]
+      };
+      res.render('hello/edit', data);
+    } 
+  });
+  connection.end();
+});
+
+router.post('/edit', (req, res, next)=> {
+  var id = req.body.id;
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = {'name': nm, 'mail': ml, 'age': ag};
+
+  var connection = mysql.createConnection(mysql_setting);
+  connection.connect();
+  connection.query('update mydata set ? where id = ?', [data, id], function (error, results, fields) {
+    res.redirect('/hello');
+  });
+  connection.end();
 });
 module.exports = router;
