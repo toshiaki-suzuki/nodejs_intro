65a6b5e5cdb0b6208de473e6b8e2125fab3fccb5
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-15 14:46:12 +0900

    use comparison operator in users/index

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
-
+const {Op} = require('sequelize');
 
 /* GET users listing. */
 router.get('/', (req, res, next)=> {
   const id = req.query.id;
   db.User.findAll({
     where: {
-      id: id
+      id: {[Op.lte]:id}
     }
   }).then(usrs => {
     var data = {
