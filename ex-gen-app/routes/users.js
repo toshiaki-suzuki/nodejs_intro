d73a5e17d5c1943e0293993e4721ba8f050f9a43
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-15 14:52:25 +0900

    use multiple comparison operator in users/index

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -5,10 +5,11 @@ const {Op} = require('sequelize');
 
 /* GET users listing. */
 router.get('/', (req, res, next)=> {
-  const nm = req.query.name;
+  const min = req.query.min;
+  const max = req.query.max;
   db.User.findAll({
     where: {
-      name: {[Op.like]: '%' + nm + '%'}
+      age: {[Op.gte]: min, [Op.lte]: max}
     }
   }).then(usrs => {
     var data = {
