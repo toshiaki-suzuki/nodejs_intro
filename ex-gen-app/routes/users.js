cd1a122e70b3acf036a0b774a7a54ab4ec553a11
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-15 10:27:57 +0900

    make users/index

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next)=> {
+  db.User.findAll().then(usrs => {
+    var data = {
+      title: 'Users/Index',
+      content: usrs
+    }
+    res.render('users/index', data);
+  })
 });
 
 module.exports = router;
