55d06fe87e0174052987388d6d9da99ea28d885c
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-15 14:41:55 +0900

    find data by id in users/index

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -5,7 +5,12 @@ const db = require('../models/index');
 
 /* GET users listing. */
 router.get('/', (req, res, next)=> {
-  db.User.findAll().then(usrs => {
+  const id = req.query.id;
+  db.User.findAll({
+    where: {
+      id: id
+    }
+  }).then(usrs => {
     var data = {
       title: 'Users/Index',
       content: usrs
