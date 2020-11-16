94c054726405108705493aeb4a616ecb39476c19
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-16 19:52:04 +0900

    make add functionality in users/add

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -7,20 +7,35 @@ const {Op} = require('sequelize');
 router.get('/', (req, res, next)=> {
   const nm = req.query.name;
   const ml = req.query.mail;
-  db.User.findAll({
-    where: {
-      [Op.or]:[
-        {name: {[Op.like]:'%' + nm +'%'}},
-        {mail: {[Op.like]:'%' + ml +'%'}}
-      ]
-    }
-  }).then(usrs => {
+  db.User.findAll()
+  .then(usrs => {
     var data = {
       title: 'Users/Index',
       content: usrs
     }
     res.render('users/index', data);
   })
+});
+
+router.get('/add', (req, res, next)=> {
+  var data = {
+    title: 'User/Add'
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add', (req, res, next)=> {
+  db.sequelize.sync()
+  .then(()=> db.User.create({
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age,
+  })
+  .then(()=> {
+    res.redirect('/users');
+  })
+  );
 });
 
 module.exports = router;
