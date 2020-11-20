cb8ed2a84f42b7d88af46c19eecbcde6ba76e3d3
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-20 14:52:57 +0900

    make home

--- favoard/routes/index.js
+++ favoard/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Favoard' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Sign Up' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Sign In' });
 });
 
 module.exports = router;
