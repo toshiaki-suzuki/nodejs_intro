ea255fd5b21d045a860ab7c03cb0232d7dd9806e
Toshiaki Suzuki <flaflabell@gmail.com>
2020-11-20 18:16:02 +0900

    make articles page

--- favoard/routes/index.js
+++ favoard/routes/index.js
@@ -9,8 +9,16 @@ router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Sign Up' });
 });
 
+router.post('/signup', function(req, res, next) {
+  res.redirect('/articles');
+});
+
 router.get('/signin', function(req, res, next) {
   res.render('signin', { title: 'Sign In' });
+});
+
+router.post('/signin', function(req, res, next) {
+  res.redirect('/articles/');
 });
 
 module.exports = router;
