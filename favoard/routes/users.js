cf2c7df9c8731e49c423a1d597141b85d52c3f7d
Toshiaki Suzuki <flaflabell@gmail.com>
2020-12-01 09:06:07 +0900

    make user detail page

--- favoard/routes/users.js
+++ favoard/routes/users.js
@@ -13,4 +13,18 @@ router.get('/', function(req, res, next) {
     res.render('users/index', data);
   });
 });
+
+router.get('/:userId', (req, res, next) => {
+  const id = req.params.userId;
+  db.Users.findAll({
+    where: { id }
+  }).then(usr => {
+    console.log(usr);
+    const userData = usr[0].dataValues;
+    var data = {
+      content: userData
+    }
+    res.render('users/user_detail', data);
+  });
+});
 module.exports = router;
