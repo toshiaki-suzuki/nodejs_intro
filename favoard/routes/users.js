1db7e4d75bf4fc3fccdb82ebf57877888c5decf0
Toshiaki Suzuki <flaflabell@gmail.com>
2020-12-13 13:28:06 +0900

    make create user page

--- favoard/routes/users.js
+++ favoard/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
 const {Op} = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/', function(req, res, next) {
   db.Users.findAll()
@@ -12,6 +13,20 @@ router.get('/', function(req, res, next) {
     }
     res.render('users/index', data);
   });
+});
+
+router.get('/create', (req, res, next)=> {
+  res.render('users/user_create');
+});
+
+router.post('/create', (req, res, next)=> {
+  db.Users.create({
+    id: uuidv4(),
+    name: req.body.name,
+    mail: req.body.mail,
+    password: req.body.password,
+    createdAt: new Date(),
+  }).then(usr => res.redirect(`/users/${usr.id}`));
 });
 
 router.get('/:userId', (req, res, next) => {
@@ -44,6 +59,7 @@ router.post('/:userId/edit', (req, res, next)=> {
     usr.name = req.body.name;
     usr.mail = req.body.mail;
     usr.password = req.body.password;
+    usr.updatedAt = new Date();
     usr.save().then(()=>res.redirect(`/users/${id}`));
   });
 });
