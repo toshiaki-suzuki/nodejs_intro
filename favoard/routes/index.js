cd1cde89e09650910d03f57928338f6f3c02884e
Toshiaki Suzuki <flaflabell@gmail.com>
2020-12-27 13:04:42 +0900

    make minimum login logic

--- favoard/routes/index.js
+++ favoard/routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models/index');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Favoard' });
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Sign Up' });
+  const data = {
+    title: 'Sign Up',
+    name: '',
+    mail: '',
+    password: ''
+  };
+  res.render('signup', data);
 });
 
 router.post('/signup', function(req, res, next) {
@@ -14,11 +21,34 @@ router.post('/signup', function(req, res, next) {
 });
 
 router.get('/signin', function(req, res, next) {
-  res.render('signin', { title: 'Sign In' });
+  const data = {
+    mail: '',
+    password: '',
+    msg: ''
+  };
+  res.render('signin', data);
 });
 
 router.post('/signin', function(req, res, next) {
-  res.redirect('articles');
+  const mail = req.body.mail;
+  const password = req.body.password;
+  db.Users.findOne({ where: { mail: mail } })
+  .then(usr => {
+    if (password === usr.password) {
+      const data = {
+        content: usr
+      }
+      res.redirect('articles');
+    } else {
+      const data = {
+        mail: mail,
+        password: '',
+        msg: 'mail or password is wrong...'
+      };
+      res.render('signin', data);
+    }
+  });
+  
 });
 
 module.exports = router;
